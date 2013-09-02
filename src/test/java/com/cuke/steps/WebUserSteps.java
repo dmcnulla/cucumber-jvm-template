2919aaf77510bdc3f044eff14ddab1eb4280c81b
Dave McNulla <mcnulla@gmail.com>
2013-09-01 23:18:09 -0700

    changed method names to be camel case.

--- src/test/java/com/cuke/steps/WebUserSteps.java
+++ src/test/java/com/cuke/steps/WebUserSteps.java
@@ -7,25 +7,24 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.assertTrue;
 
 public class WebUserSteps extends BaseUserSteps {
-    
+
     @Given("^I am a web user$")
-    public void I_am_a_web_user() throws Throwable {
+    public void iAmAWebUser() throws Throwable {
         assertTrue(user.exists());
     }
 
     @Given("^I go to Google$")
-    public void I_go_to_Google() throws Throwable {
+    public void iGoToGoogle() throws Throwable {
         user.atSearchPage().open();
     }
 
     @When("^I search for \"([^\"]*)\"$")
-    public void I_search_for(String searchString) throws Throwable {
+    public void iSearchFor(String searchString) throws Throwable {
         user.atSearchPage().searchFor(searchString);
     }
 
     @Then("^I should find \"([^\"]*)\"$")
-    public void I_should_find(String url) throws Throwable {
+    public void iShouldFind(String url) throws Throwable {
         assertTrue(user.atResultsPage().resultExists(url));
     }
-
 }
